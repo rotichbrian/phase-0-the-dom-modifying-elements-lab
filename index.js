f8dc01065e577dbbbac15985fbac40f9cb02f95f
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-04 05:17:04 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// DOM manipulation
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Effie is the champion';
